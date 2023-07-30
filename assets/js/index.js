14228fe8a5b19027da7b0efbcfc8c6e859f75ef3
Prince Tagadiya <princetagadiya11@gmail.com>
2023-07-30 13:19:29 +0530

    scroll spy fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,24 +37,83 @@ navbarLinks.forEach((link) => {
 // ===============================
 //           Scrollspy
 // ===============================
-let section = document.querySelectorAll('section');
+
+// let section = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('.sticky header nav ul li a');
+
+// window.onscroll = () => {
+//     section.forEach(sec =>{
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if(top >= offset && top < offset + height){
+//             navLinks.forEach(links =>{
+//                 links.classList.remove('active1');
+//                 document.querySelector('.sticky header nav ul li a[href*=' + id + ']').classList.add('active1');
+//             });
+//         };
+//     });
+// }
+
+let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('.sticky header nav ul li a');
+let contactLink = navLinks[navLinks.length - 1];
 
-window.onscroll = () => {
-    section.forEach(sec =>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+// Function to add or remove the 'active1' class from navigation links
+function setActiveLink(link) {
+    navLinks.forEach((navLink) => {
+        navLink.classList.remove('active1');
+    });
+    link.classList.add('active1');
+}
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links =>{
-                links.classList.remove('active1');
-                document.querySelector('.sticky header nav ul li a[href*=' + id + ']').classList.add('active1');
-            });
-        };
+// Function to handle scroll event and activate the appropriate link
+function handleScroll() {
+    let scrollPosition = window.scrollY;
+    let windowHeight = window.innerHeight;
+
+    let lastSection = sections[sections.length - 1];
+    let lastSectionOffset = lastSection.offsetTop;
+    let lastSectionHeight = lastSection.offsetHeight;
+
+    // Activate the contact link when the user scrolls to the last section
+    if (scrollPosition + windowHeight >= lastSectionOffset + lastSectionHeight) {
+        setActiveLink(contactLink);
+        return;
+    }
+
+    sections.forEach((section) => {
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+
+        if (scrollPosition >= offset && scrollPosition < offset + height) {
+            let id = section.getAttribute('id');
+            let correspondingLink = document.querySelector(`.sticky header nav ul li a[href="#${id}"]`);
+            if (correspondingLink) {
+                setActiveLink(correspondingLink);
+            }
+        }
     });
 }
+
+// Add scroll event listener to the window
+window.addEventListener('scroll', handleScroll);
+
+// Add click event listeners to navigation links for smooth scrolling
+navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        let target = document.querySelector(link.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth',
+            });
+        }
+    });
+});
 
 // ===============================
 //       CV Downlaod Button
