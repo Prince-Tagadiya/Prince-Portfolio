f5d248fe1d6c83c6a9c7ffe304aad9fc45bb4f2a
Prince Tagadiya <princetagadiya11@gmail.com>
2023-10-10 17:06:59 +0530

    Enhance Security

--- assets/js/index.js
+++ assets/js/index.js
@@ -349,7 +349,22 @@ document.onkeydown = (e) => {
   )
     return false;
 };
+// Adding `keydown` event listener on the document
+document.addEventListener('keydown', (event) => {
+  console.log(`User pressed: ${event.key}`);
+  event.preventDefault();
+  return false;
+});
 
+const inputField = document.getElementById('message');
+
+// or adding `keydown` on the `input` element
+inputField.addEventListener('keydown', (event) => {
+  console.log(`User pressed: ${event.key}`);
+  console.log('hi');
+  event.preventDefault();
+  return false;
+});
 // ===============================
 //          Title Flash
 // ===============================
